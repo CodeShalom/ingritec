e9924b8030b33f2cc28f1b31dfe87b2878bc7a86
Merky <merkychacon@hotmail.com>
2016-07-14 11:36:09 -0400

    agregar nueva ubicacion en mapa

--- contacto.php
+++ contacto.php
@@ -12,8 +12,7 @@
       <strong>Tel/Fax:</strong> 809-274-4166<br>
       <strong>Email:</strong> <a href="mailto:ingritec@gmail.com">ingritec@gmail.com</a> </p>
       
-      
-<iframe src="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d7569.101906571779!2d-69.96723255599943!3d18.45868673338865!3m2!1i1024!2i768!4f13.1!5e0!3m2!1ses-419!2sdo!4v1415848952010" width="100%" height="350" frameborder="0" style="border:0"></iframe>
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15128.894840593615!2d-70.03158819827549!3d18.56395041640215!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8eaf8b020a01ff6f%3A0x56760bbe0a817e22!2sMerca+Santo+Domingo!5e0!3m2!1ses-419!2sdo!4v1468510502255" width="100%" height="350" frameborder="0" style="border:0" allowfullscreen></iframe>
   </div>
   
   <!-- Cuerpo --> 
